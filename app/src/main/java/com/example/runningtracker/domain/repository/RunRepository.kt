2ace5282b59e592693f42233075860e0f825c2d3
ParkGiTeak <rl980901@naver.com>
2026-03-02 18:31:01 +0900

    feat: RunRepository Interface 추가

---
+++ app/src/main/java/com/example/runningtracker/domain/repository/RunRepository.kt
@@ -0,0 +1,14 @@
+package com.example.runningtracker.domain.repository
+
+import com.example.runningtracker.domain.model.Run
+import kotlinx.coroutines.flow.Flow
+
+interface RunRepository {
+    suspend fun insertRun(run: Run)
+    suspend fun deleteRun(run: Run)
+
+    fun getAllRunsSortedByDate(): Flow<List<Run>>
+    fun getAllRunsSortedByTimeInMillis(): Flow<List<Run>>
+    fun getAllRunsSortedByDistance(): Flow<List<Run>>
+    fun getAllRunsSortedByAvgSpeed(): Flow<List<Run>>
+}
