018f595a124dc56deaf4bc5c5db68af5e0363889
ParkGiTeak <rl980901@naver.com>
2026-03-02 19:45:05 +0900

    feat: Koin AppModule 등록
    - RunningTrackerApplication에 appModule 등록 추가
    - AndroidManifest Application name 추가

--- app/src/main/java/com/example/runningtracker/RunningTrackerApplication.kt
+++ app/src/main/java/com/example/runningtracker/RunningTrackerApplication.kt
@@ -1,6 +1,7 @@
 package com.example.runningtracker
 
 import android.app.Application
+import com.example.runningtracker.core.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -11,6 +12,7 @@ class RunningTrackerApplication: Application() {
         startKoin {
             androidLogger()
             androidContext(this@RunningTrackerApplication)
+            modules(appModule)
         }
     }
 }
