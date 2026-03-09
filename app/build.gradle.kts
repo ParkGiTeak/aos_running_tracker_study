c5bf7f15c412ea38891082efc8559ae6dffcdf2c
ParkGiTeak <rl980901@naver.com>
2026-03-09 22:22:07 +0900

    feat: Compose Material Icons Extended 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,7 @@ dependencies {
     implementation(libs.koin.core)
     implementation(libs.koin.android)
     implementation(libs.koin.androidx.compose)
+
+    // MaterialIcons
+    implementation(libs.androidx.compose.material.icons.extended)
 }
